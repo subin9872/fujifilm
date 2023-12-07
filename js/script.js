80aa5adce1d66a01acf8736924e80ca248a1cbff
subin9872 <153070748+subin9872@users.noreply.github.com>
2023-12-07 15:20:46 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('.new-wrapper').bxSlider({
+        minSlides: 3,
+        maxSlides: 5,
+        slideWidth: 200,
+        slideMargin: 300,
+        pager: false,
+        moveSlides: 1,
+    });
+});
